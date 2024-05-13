37f2e25f2a058d182cd829b61d0653fa4af8d370
yotu <114195789+ulxsth@users.noreply.github.com>
2024-05-13 10:31:43 +0900

    upd: authのルーティング作成

--- routes/auth.js
+++ routes/auth.js
@@ -5,4 +5,12 @@ router.get("/signin", (req, res) => {
   res.render("auth/signin");
 })
 
+router.get("/signup", (req, res) => {
+  res.render("auth/signup");
+})
+
+router.post("confirm_email", (req, res) => {
+  res.render("auth/confirm_email");
+})
+
 module.exports = router;
