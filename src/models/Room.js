6c73cadc78a2ad41d51ae2a31af122df501b8d10
yotu <114195789+ulxsth@users.noreply.github.com>
2024-05-14 12:34:30 +0900

    upd: update 下書き

---
+++ src/models/Room.js
@@ -0,0 +1,13 @@
+export class Room {
+  /**
+   * ルーム情報を更新する。
+   * @param {string} id ルームID
+   * @param {Object} data 更新するルーム情報
+   * @returns {boolean} 更新に成功したか
+   */
+  static update(id, data) {
+    // ↓↓↓ ここに処理を書く ↓↓↓
+
+    // ↑↑↑ ここに処理を書く ↑↑↑
+  }
+}
