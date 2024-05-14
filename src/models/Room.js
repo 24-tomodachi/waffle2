fc724490594edd617eb5c535e57721c8ebdd21d0
yotu <114195789+ulxsth@users.noreply.github.com>
2024-05-14 11:52:13 +0900

    upd: Roomモデル作成、create下書き

---
+++ src/models/Room.js
@@ -0,0 +1,13 @@
+export class Room {
+
+  /**
+   * 新しくルームを作成する。
+   * @param {String} name
+   * @param {String} description
+   */
+  create = (name, description) => {
+    // ↓↓↓ ここに処理を書く
+
+    // ↑↑↑ ここに処理を書く
+  }
+}
