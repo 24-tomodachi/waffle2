9e1a19f7229c6a059b7cc1b34c0fe31fe6a4bcb2
yotu <114195789+ulxsth@users.noreply.github.com>
2024-05-14 12:10:59 +0900

    upd: findById下書き

---
+++ src/models/Room.js
@@ -0,0 +1,12 @@
+export class Room {
+  /**
+   * ルームidをもとに、ルームを検索する。
+   * @param {string} id ルームid
+   * @returns {Object} ルーム
+   */
+  static findById(id) {
+    // ↓↓↓ ここに処理を書く
+
+    // ↑↑↑ ここに処理を書く
+  }
+}
