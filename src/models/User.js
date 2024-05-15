bc6da7056da0dfeb8fdb5b9ce03650e78db6b854
yotu <114195789+ulxsth@users.noreply.github.com>
2024-05-15 11:00:11 +0900

    upd: User.create 下書き

---
+++ src/models/User.js
@@ -0,0 +1,14 @@
+export class User {
+  /**
+   * ユーザーを作成する。
+   * @param {string} name ユーザー名
+   * @param {string} email メールアドレス
+   * @param {string} password_hash パスワード
+   * @returns {string} 作成したユーザーのID
+   */
+  static create(name, email, password_hash) {
+    // ↓↓↓ ここに処理を書く ↓↓↓
+
+    // ↑↑↑ ここに処理を書く ↑↑↑
+  }
+}
