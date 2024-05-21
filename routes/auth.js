6a12f3b8a402deb19779e24b525d8720b23781fc
Ryotoku <2201024@s.asojuku.ac.jp>
2024-05-21 10:58:51 +0900

    処理の流れをコメントに書いた

--- routes/auth.js
+++ routes/auth.js
@@ -9,9 +9,14 @@ router.get("/signup", (req, res) => {
   res.render("auth/signup");
 })
 
-router.post("/signup", () => {
-  // postされたときにしたいこと
+router.post("/signup", (req, res) => {
+  // 新しいユーザーをデータベースに作成する
+  
+
+  // サインアップ成功ページにリダイレクトする
+  res.redirect('/');
 });
+
 
 router.get("/confirm_email", (req, res) => {
   res.render("auth/confirm_email");
