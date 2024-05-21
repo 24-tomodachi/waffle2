f0f7e1a97d271382f01782f9a82faa4dee0cb57c
yotu <114195789+ulxsth@users.noreply.github.com>
2024-05-21 12:19:08 +0900

    fix: 分割代入してなかった

--- src/libs/supabase.js
+++ src/libs/supabase.js
@@ -1,4 +1,4 @@
-const createClient = require('subapase');
+const { createClient } = require('@supabase/supabase-js');
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_KEY
