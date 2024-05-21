4cb36d1a8493cc89ffb2f19f52de747293b7dcc5
yotu <114195789+ulxsth@users.noreply.github.com>
2024-05-21 11:59:59 +0900

    ref: UserModelがすべての関数を持つように

--- src/models/User.js
+++ src/models/User.js
@@ -1,12 +1,11 @@
-export class User {
+const UserModel = {
   /**
-   * ユーザーを作成する。
    * @param {string} name ユーザー名
    * @param {string} email メールアドレス
    * @param {string} password_hash パスワード
    * @returns {string} 作成したユーザーのID
    */
-  static async create(name, email, password_hash) {
+  create: async (name, email, password_hash) => {
     const { data, error } = await supabase
       .from('users')
       .insert([
@@ -20,3 +19,5 @@ export class User {
     return data[0].id;
   }
 }
+
+module.exports = UserModel;
