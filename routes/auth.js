c0292c027c5ffdd525977e7f5b62ac02b7a79e2d
yotu <114195789+ulxsth@users.noreply.github.com>
2024-06-03 12:36:44 +0900

    Revert "#13 : ユーザー登録機能を作成した"

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../src/models/User');
-const bcrypt = require('bcrypt');
 
 router.get("/signin", (req, res) => {
   res.render("auth/signin");
@@ -10,25 +8,6 @@ router.get("/signin", (req, res) => {
 router.get("/signup", (req, res) => {
   res.render("auth/signup");
 })
-
-router.post("/signup", async (req, res) => {
-  const email = req.body.email
-  const password = req.body.password
-
-  // emailが存在してたら処理を中断
-  if(await User.findByEmail(email)) {
-    return res.redirect("/auth/signup");
-  }
-
-  const salt = await bcrypt.genSalt(10);
-  const password_hash = await bcrypt.hash(password, salt)
-
-  User.create(email, password_hash);
-  
-  // サインアップ成功ページにリダイレクトする
-  res.redirect('/');
-});
-
 
 router.get("/confirm_email", (req, res) => {
   res.render("auth/confirm_email");
