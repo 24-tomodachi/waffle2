47a60a7ad1d11201311ba0ef9b44d51cfbbb7cdf
Gingira1213 <gin12132828@icloud.com>
2024-06-05 11:40:04 +0900

    upd: roomのエラー修正

--- src/models/Room.js
+++ src/models/Room.js
@@ -1,13 +1,21 @@
-export class Room {
+const supabase = require('../libs/supabase');
 
+const RoomModel = {
   /**
    * 新しくルームを作成する。
    * @param {String} name
    * @param {String} description
    */
-  create = (name, description) => {
-    // ↓↓↓ ここに処理を書く
+  create: async (name, userId, description="") => {
+    const { data, error } = await supabase
+    .from('rooms')
+    .insert([
+        { name, user_id:userId, description }
+    ])
+    .select('*');
 
     // ↑↑↑ ここに処理を書く
   }
 }
+
+module.exports = RoomModel;
