555448d9dc2a573571caaa009e46afbb8d0d10ad
Gingira1213 <gin12132828@icloud.com>
2024-06-05 10:32:05 +0900

    upd: Room.jsのルーム概要(discription)

--- src/models/Room.js
+++ src/models/Room.js
@@ -5,14 +5,15 @@ const RoomModel = {
    * ルームを作成する
    * @param {string} name ルーム名
    * @param {number} userId 作成者
+   * @param {string} [discription=""] ルーム概要
    * @returns {Object} 作成されたデータを返す
    * @throws {Error} DB操作に失敗した場合
    */
-  create: async (name, userId) => {
+  create: async (name, userId, description="") => {
     const { data, error } = await supabase
     .from('rooms')
     .insert([
-        { name, userId }
+        { name, userId ,description }
     ])
     .select('*');
 
@@ -20,8 +21,7 @@ const RoomModel = {
         throw new Error(`Room creation failed: ${error.message}`);
       }
       return data[0];
-  },
-  
+  }
 }
 
 module.exports = RoomModel;
