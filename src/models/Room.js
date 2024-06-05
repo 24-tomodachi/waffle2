dc46f448a1fa78186daef2f80b155454054923de
Gingira1213 <gin12132828@icloud.com>
2024-06-05 11:56:55 +0900

    upd: roomのtest

--- src/models/Room.js
+++ src/models/Room.js
@@ -3,8 +3,11 @@ const supabase = require('../libs/supabase');
 const RoomModel = {
   /**
    * 新しくルームを作成する。
-   * @param {String} name
-   * @param {String} description
+   * @param {string} name　ルーム名
+   * @param {int} userId　作成者
+   * @param {string} [description=""] ルーム概要
+   * @returns {string} 作成したユーザーのid
+   * @throws {Error} DB操作に失敗した場合
    */
   create: async (name, userId, description="") => {
     const { data, error } = await supabase
@@ -14,8 +17,11 @@ const RoomModel = {
     ])
     .select('*');
 
-    // ↑↑↑ ここに処理を書く
+    if (error) {
+        throw new Error(`Room creation failed: ${error.message}`);
+      }
+      return data[0];
+    }
   }
-}
 
 module.exports = RoomModel;
