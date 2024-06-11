6008edd4393cda7b96f14b624bcd91ae29252550
Gingira1213 <gin12132828@icloud.com>
2024-06-11 10:25:12 +0900

    upd: seed に rooms を追加

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -9,3 +9,15 @@ INSERT INTO users (email, password_hash, name, twitter_id) VALUES
     ('user8@example.com', 'password8', 'User 8', 'twitter8'),
     ('user9@example.com', 'password9', 'User 9', 'twitter9'),
     ('user10@example.com', 'password10', 'User 10', 'twitter10');
+
+INSERT INTO rooms (name, user_id, description) VALUES
+    ('Room 1', 1, 'Room 1 description'),
+    ('Room 2', 2, 'Room 2 description'),
+    ('Room 3', 3, 'Room 3 description'),
+    ('Room 4', 4, 'Room 4 description'),
+    ('Room 5', 5, 'Room 5 description'),
+    ('Room 6', 6, 'Room 6 description'),
+    ('Room 7', 7, 'Room 7 description'),
+    ('Room 8', 8, 'Room 8 description'),
+    ('Room 9', 9, 'Room 9 description'),
+    ('Room 10', 10, 'Room 10 description');
