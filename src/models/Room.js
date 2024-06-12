e853848a9da6351837415596a5eeffeb741592e9
Gingira1213 <gin12132828@icloud.com>
2024-06-12 09:39:14 +0900

    ルーム作成機能競合直し

--- src/models/Room.js
+++ src/models/Room.js
@@ -13,17 +13,15 @@ const RoomModel = {
         throw new Error(`Room creation failed: ${error.message}`);
       }
       return data[0];
-    }
-  },
+    },
 
-  export class Room {
     /**
      * ルーム情報を更新する。
      * @param {string} id ルームID
      * @param {Object} data 更新するルーム情報
      * @returns {boolean} 更新に成功したか
      */
-    static update(id, data) {
+    update: async (id, data) => {
       // ↓↓↓ ここに処理を書く ↓↓↓
   
       // ↑↑↑ ここに処理を書く ↑↑↑
