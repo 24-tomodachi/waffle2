da4a0d38488ff0e3487e58633b8c248705ce5e03
yotu <114195789+ulxsth@users.noreply.github.com>
2024-06-12 12:16:17 +0900

    refactor

--- src/models/Room.js
+++ src/models/Room.js
@@ -9,39 +9,38 @@ const RoomModel = {
    * @returns {Object} 作成したルーム
    * @throws {Error} ルームの作成に失敗した場合
    */
-  create: async (name, userId, description="") => {
+  create: async (name, userId, description = "") => {
     const { data, error } = await supabase
-    .from('rooms')
-    .insert([
-        { name, user_id:userId, description }
-    ])
-    .select('*');
+      .from('rooms')
+      .insert([
+        { name, user_id: userId, description }
+      ])
+      .select('*');
 
     if (error) {
-        throw new Error(`Room creation failed: ${error.message}`);
-      }
-      return data[0];
-    },
-    /**
-     * ルーム情報を更新する。
-     * @param {number} id ルームID
-     * @param {Object} updateData 更新するルーム情報
-     * @returns {Object} 更新されたデータ
-     */
-    update: async (id, updateData) => {
-    const { data, error } = await supabase
-    .from('rooms')
-    .update(updateData)
-    .eq('id', id)
-    .select();
-
-    if (error) {
-        throw new Error(`Room creation failed: ${error.message}`);
+      throw new Error(`Room creation failed: ${error.message}`);
     }
     return data[0];
+  },
+
+  /**
+   * ルーム情報を更新する。
+   * @param {number} id ルームID
+   * @param {Object} updateData 更新するルーム情報
+   * @returns {Object} 更新されたデータ
+   */
+  update: async (id, updateData) => {
+    const { data, error } = await supabase
+      .from('rooms')
+      .update(updateData)
+      .eq('id', id)
+      .select();
+
+    if (error) {
+      throw new Error(`Room creation failed: ${error.message}`);
     }
-  }
-    },
+    return data[0];
+  },
 
   /**
    * ルームidをもとに、ルームを検索する。
@@ -49,7 +48,7 @@ const RoomModel = {
    * @returns {Object} ルーム
    * @throws {Error} ルームの検索に失敗した場合
    */
-  findById: async(id) => {
+  findById: async (id) => {
     const { data, error } = await supabase
       .from('rooms')
       .select('*')
