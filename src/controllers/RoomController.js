e29367c44dd0472de202c1e7c8bced889ae2caa3
yotu <114195789+ulxsth@users.noreply.github.com>
2024-06-19 09:46:44 +0900

    upd: RoomController.create を実装

---
+++ src/controllers/RoomController.js
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import RoomModel from "../models/Room.js";
+
+const RoomController = {
+  /**
+   * ルームを作成する。
+   * @param {Request} req
+   * @param {Response} res
+   */
+  create: async (req, res) => {
+    const { name } = req.body;
+    const userId = req.session.userId;
+    if (!userId) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const room = await RoomModel.create(name, userId);
+
+    res.status(201).json(room);
+  },
+};
