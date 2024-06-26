5a2eae9c4d10d765c7fcfcf2bcf2a43b10b4e1ee
ayyn0863 <140891515+ayyn0863@users.noreply.github.com>
2024-06-26 10:50:16 +0900

    faviconの導入

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var favicon = require('serve-favicon')
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -32,6 +33,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname,'public','favicon.ico')));
+
 
 app.use('/', indexRouter);
 app.use('/auth', authRouter);
