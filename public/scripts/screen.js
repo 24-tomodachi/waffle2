2526629605cb6a68d082f78d9405f2c582f44eb9
yotu <114195789+ulxsth@users.noreply.github.com>
2024-07-02 09:54:47 +0900

    move: obstacles.jsonをpublicに移動

--- public/scripts/screen.js
+++ public/scripts/screen.js
@@ -1,14 +1,26 @@
 const canvas = document.getElementById("screen");
 const ctx = canvas.getContext("2d");
 
+let obstacles;
+
 const draw = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillText("Hello World", 100, 100);
+  obstacles.forEach((obstacle) => {
+    ctx.fillRect(obstacle.x, obstacle.y, obstacle.width, obstacle.height);
+  });
   window.requestAnimationFrame(draw);
 }
 
-const init = () => {
+const init = async () => {
+  obstacles = await loadObstacles();
   draw();
 }
+
+const loadObstacles = async () => {
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return await response.json();
+};
 
 init();
