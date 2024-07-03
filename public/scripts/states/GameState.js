6ab36f3d4ef409f0d6db71b5adfbed9070de85f4
yotu <114195789+ulxsth@users.noreply.github.com>
2024-07-03 09:52:48 +0900

    feat: isCollidingWith

--- public/scripts/states/GameState.js
+++ public/scripts/states/GameState.js
@@ -20,4 +20,18 @@ export default class GameState {
   findByClass(cls) {
     return this.objects.filter((object) => object instanceof cls);
   }
+
+  isCollidingWith = (obj) => {
+    const isXOverlapped = this.x < (obj.x + obj.width) &&
+      (this.x + this.width) > obj.x ||
+      obj.x < (this.x + this.width) &&
+      (obj.x + obj.width) > this.x;
+
+    const isYOverlapped = this.y < (obj.y + obj.height) &&
+      (this.y + this.height) > obj.y ||
+      obj.y < (this.y + this.height) &&
+      (obj.y + obj.height) > this.y;
+
+    return isXOverlapped && isYOverlapped;
+  };
 }
