599e9c271f1a07665bf3449ac404d90a545285ad
yotu <114195789+ulxsth@users.noreply.github.com>
2024-07-03 09:31:20 +0900

    feat: InteractionState

---
+++ public/scripts/states/InteractionState.js
@@ -0,0 +1,36 @@
+export default class InteractionState {
+  constructor() {
+    this.interactFlags = {
+      up: false,
+      down: false,
+      left: false,
+      right: false
+    };
+  }
+
+  /**
+   * 特定のフラグが立っているかを取得する。
+   * @param {string} flag フラグ名
+   * @returns {boolean} フラグが立っているか
+   */
+  isFlag(flag) {
+    if (this.interactFlags.hasOwnProperty(flag)) {
+      return this.interactFlags[flag];
+    } else {
+      throw new Error(`Flag "${flag}" is not defined.`);
+    }
+  }
+
+  /**
+   * フラグを更新する。
+   * @param {string} flag フラグ名
+   * @param {boolean} value フラグの値
+   */
+  setFlag(flag, value) {
+    if (this.interactFlags.hasOwnProperty(flag)) {
+      this.interactFlags[flag] = value;
+    } else {
+      throw new Error(`Flag "${flag}" is not defined.`);
+    }
+  }
+}
