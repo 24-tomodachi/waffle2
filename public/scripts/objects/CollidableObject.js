60e58c997245cd60936f2acff32f0b842737a998
yotu <114195789+ulxsth@users.noreply.github.com>
2024-07-03 10:05:03 +0900

    upd: CollidableObject

--- public/scripts/objects/CollidableObject.js
+++ public/scripts/objects/CollidableObject.js
@@ -1,5 +1,7 @@
-import GameObject from "./GameObject";
+import GameObject from "./GameObject.js";
 
 export default class CollidableObject extends GameObject {
-  
+  constructor(x, y, width, height, imgPath) {
+    super(x, y, width, height, imgPath);
+  }
 }
