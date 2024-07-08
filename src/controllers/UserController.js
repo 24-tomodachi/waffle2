e9e7595a249cd592b06ced317a9c11d8d7c6a391
Gingira1213 <gin12132828@icloud.com>
2024-07-08 09:37:04 +0900

    upd:usercontroller 途中

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -3,22 +3,21 @@ const RoomModel = require("../models/User");
 
 const UserController = {
     /**
-     * ルームを作成する。
-     * @param {Request} req
-     * @param {Response} res
-     */
-    create: async (req, res) => {
-      const { name, description } = req.body;
-      const userId = req.userId;
-      if (!userId) { 
-        return res.status(401).json({ message: "Unauthorized" });
-      }
-  
-      const room = await RoomModel.create(name, userId, description);
-  
-      res.status(201).redirect("/rooms/" + room.id);
-    },
-  
+   * ユーザーの情報を更新する。
+   * @param {Request} req
+   * @param {Response} res
+   */
+  update: async (req, res) => {
+    const returned_at = req.body;
+    const userId = req.userId;
+    if (!userId) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    await RoomModel.update(userId,returned_at);
+
+    res.status(201).redirect("/rooms/select-mode/");
+  }
   }
   
   module.exports = UserController
