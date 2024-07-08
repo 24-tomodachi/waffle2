7af4ae6f6bc999503b72af1bc189edfca1c69bfe
Gingira1213 <gin12132828@icloud.com>
2024-07-08 10:46:14 +0900

    upd:ユーザーの名前・説明　 遷移ができたとこまで

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,5 +1,5 @@
 const { Request, Response } = require("express");
-const RoomModel = require("../models/User");
+const UserModel = require("../models/User");
 
 const UserController = {
     /**
@@ -8,16 +8,16 @@ const UserController = {
    * @param {Response} res
    */
   update: async (req, res) => {
-    const returned_at = req.body;
+    const { name, description } = req.body;
     const userId = req.userId;
     if (!userId) {
       return res.status(401).json({ message: "Unauthorized" });
-    }
+      }
 
-    await RoomModel.update(userId,returned_at);
+    await UserModel.updateById(userId,{name,description});
 
     res.status(201).redirect("/rooms/select-mode/");
-  }
+    }
   }
   
   module.exports = UserController
