30eee92e2645d533f6bb667408caec15ddffc344
yotu <114195789+ulxsth@users.noreply.github.com>
2024-07-09 10:28:28 +0900

    feat: リアクションが一定時間で消えるようにした

--- public/scripts/objects/Reaction.js
+++ public/scripts/objects/Reaction.js
@@ -1,9 +1,14 @@
 import GameObject from "./GameObject.js";
+import { gameState } from "../screen.js";
 
 export class Reaction extends GameObject {
   constructor(player, imgPath) {
     super(player.x + player.width / 2, player.y - player.height / 2, 30, 30, imgPath);
     this.player = player;
+
+    setTimeout(() => {
+      gameState.removeObject(this);
+    }, 1200);
   }
 
   update() {
