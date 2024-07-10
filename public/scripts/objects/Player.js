5730c2aaa88154cbe5a03c9883604a4745fdf518
Gingira1213 <gin12132828@icloud.com>
2024-07-10 11:37:45 +0900

    upd:画面の縦を 1000→420 Player.jsのupdate関数で画面の外に行かないように実装

--- public/scripts/objects/Player.js
+++ public/scripts/objects/Player.js
@@ -2,6 +2,8 @@ import GameObject from "./GameObject.js";
 import InteractionState from "../states/InteractionState.js";
 
 export const SPEED = 3;
+const SCREEN_WIDTH = 1000; // ゲーム画面の幅
+const SCREEN_HEIGHT = 420; // ゲーム画面の高さ
 
 export default class Player extends GameObject {
   constructor(id, x, y, width, height, imgPath) {
@@ -13,13 +15,21 @@ export default class Player extends GameObject {
 
   update() {
     if (this.interactionState.getFlag("up")) {
-      this.y -= SPEED;
+      if (this.y - SPEED >= 0) { // 上端に達していない場合のみ移動
+        this.y -= SPEED;
+      }
     } else if (this.interactionState.getFlag("down")) {
-      this.y += SPEED;
+      if (this.y + this.height + SPEED <= SCREEN_HEIGHT) { // 下端に達していない場合のみ移動
+        this.y += SPEED;
+      }
     } else if (this.interactionState.getFlag("left")) {
-      this.x -= SPEED;
+      if (this.x - SPEED >= 0) { // 左端に達していない場合のみ移動
+        this.x -= SPEED;
+      }
     } else if (this.interactionState.getFlag("right")) {
-      this.x += SPEED;
+      if (this.x + this.width + SPEED <= SCREEN_WIDTH) { // 右端に達していない場合のみ移動
+        this.x += SPEED;
+      }
     }
   }
 }
