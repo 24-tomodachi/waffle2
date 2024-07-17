8068caf2ba491fa9f514677282c495ce3e1e28d3
yotu <114195789+ulxsth@users.noreply.github.com>
2024-07-17 10:45:55 +0900

    feat: base64-decode を使用して画像をデコードするように

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -3,6 +3,7 @@ const UserModel = require("../models/User");
 const fs = require('fs').promises;
 const path = require('path');
 const ProfileImageModel = require("../models/ProfileImage");
+const { decode } = require("base64-arraybuffer");
 
 const UserController = {
     /**
@@ -17,33 +18,15 @@ const UserController = {
       return res.status(401).json({ message: "Unauthorized" });
     }
 
-    // TODO: 画像は変更しない場合にも対応
-    console.log(req.file);
-    let profilePicturePath, tempPath;
-    if (req.file) {
-      tempPath = req.file.path;
-      const targetPath = path.join(__dirname, "../../public/uploads/", req.file.originalname);
-
-      try {
-        await fs.rename(tempPath, targetPath);
-      } catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: "File upload failed" });
-      }
-
-      profilePicturePath = `/uploads/${req.file.originalname}`;
-    }
-
     // TODO: S3 にアップロード
-    console.log(req.file);
-    await ProfileImageModel.upload(req.file);
+    ProfileImageModel.upload(req.file);
 
     // TODO: upload が終わったらサーバ上から削除
     if (tempPath) {
-      await fs.unlink(tempPath);
+      fs.unlink(tempPath);
     }
 
-    await UserModel.updateById(userId,{name,description});
+    UserModel.updateById(userId,{name,description});
 
     res.status(201).redirect("/rooms/select-mode/");
     }
--- src/models/ProfileImage.js
+++ src/models/ProfileImage.js
@@ -1,11 +1,14 @@
+const { decode } = require("base64-arraybuffer");
 const supabase = require("../libs/supabase.js");
 const path = require("path");
+const fs = require("fs").promises;
 
 const ProfileImageModel = {
   async upload(file) {
+    const fileBase64 = decode(file.buffer.toString("base64"));
     const { error } = await supabase.storage
       .from('profile_images')
-      .upload(`/uploads/${file.originalname}`, path.join(__dirname, "../../public/uploads/", file.originalname));
+      .upload(`/uploads/${file.originalname}`, fileBase64);
     if (error) {
       console.error("Error uploading profile image: " + error.message);
     }
